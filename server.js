c7a23337f241ef1ac56820908d20e7decb259c9c
Mitko Razboynikov <mitkoo@gmail.com>
2016-02-10 06:35:34 +0000

    I want to add basic routing to the server

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
+var connect = require('connect');
 var http = require('http');
-http.createServer(function (req, res) {
+
+var app = connect();
+
+// Not workie: http://www.senchalabs.org/connect/proto.html
+app.use('/api', function fooMiddleware(req, res, next) {
+    // res.write("req.url: " + req.url + "\n\n");
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
-}).listen(process.env.PORT, process.env.IP);
+    res.end('api call ' + req.url + '\n');
+    next();
+});
+app.use('/ui', function barMiddleware(req, res, next) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('index.html\n');
+    next();
+});
+
+http.createServer(app)
+    .listen(process.env.PORT, process.env.IP);
