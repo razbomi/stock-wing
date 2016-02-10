7fdebb3acf14653b9fb312280ce33ca78f5ad69d
Mitko Razboynikov <mitkoo@gmail.com>
2016-02-10 16:03:33 +0000

    Added a nodejs super simple proxy

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ Playing the [stockfighter](https://www.stockfighter.io/ui/ "Stockfigter UI") gam
 1. Use the temporary key as described [here...](https://starfighter.readme.io/docs/getting-started "Getting Started")
 2. Getting help [here...](https://discuss.starfighters.io/ "Starfigter Forum")  
 3. Documentation [here..](https://starfighter.readme.io/ "Developer Hub")
+
+### References
+* https://github.com/senchalabs/connect
+* https://github.com/nodejitsu/node-http-proxy
--- server.js
+++ server.js
@@ -1,20 +1,23 @@
-var connect = require('connect');
 var http = require('http');
+var connect = require('connect');
+var serveStatic = require('serve-static');
+var httpProxy = require('http-proxy');
 
 var app = connect();
+var proxy = httpProxy.createProxyServer();
 
-// Not workie: http://www.senchalabs.org/connect/proto.html
-app.use('/api', function fooMiddleware(req, res, next) {
-    // res.write("req.url: " + req.url + "\n\n");
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('api call ' + req.url + '\n');
-    next();
+// Routes: Should you put in modules
+app.use('/api', function fooMiddleware(req, res) {
+    proxy.web(req, res, { target: 'https://api.stockfighter.io/ob/api', secure: false });
 });
-app.use('/ui', function barMiddleware(req, res, next) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('index.html\n');
-    next();
+
+// Not useful: It redirects for some reason?
+app.use('/ui', function barMiddleware(req, res) {
+    proxy.web(req, res, { target: 'https://www.stockfighter.io/ui', secure: false });
 });
+
+// What about permissions
+app.use(serveStatic(__dirname));
 
 http.createServer(app)
     .listen(process.env.PORT, process.env.IP);
