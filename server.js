51f72a715bdbb81c60aabf595e9fe2dfa2b1bcf5
Mitko Razboynikov <mitkoo@gmail.com>
2016-07-25 16:55:52 +0000

    Proxy wanted origin

--- server.js
+++ server.js
@@ -1,21 +1,30 @@
 var http = require('http');
 var connect = require('connect');
+var morgan = require('morgan');
 var serveStatic = require('serve-static');
 var httpProxy = require('http-proxy');
 
+// https://github.com/nodejitsu/node-http-proxy
 var proxy = httpProxy.createProxyServer();
-
-var app = connect();
-// app.use( connect.logger() );
-
-// Routes: Should you put in modules
-app.use('/api', function fooMiddleware(req, res) {
-    proxy.web(req, res, { target: 'https://api.stockfighter.io/ob/api', secure: false });
+proxy.on('error', function (err, req, res) {
+  res.writeHead(500, {'Content-Type': 'text/plain'});
+  res.end('Error');
 });
 
-// Not useful: It redirects for some reason?
-app.use('/ui', function barMiddleware(req, res) {
-    proxy.web(req, res, { target: 'https://www.stockfighter.io/ui', secure: false });
+// Middleware: https://github.com/senchalabs/connect#readme
+var app = connect();
+
+// Logger: https://www.npmjs.com/package/morgan
+app.use( morgan('dev') );
+
+// Routes: Should you put in modules
+app.use('/ob', function fooMiddleware(req, res, next) {
+    proxy.web(req, res, { target: 'https://api.stockfighter.io/ob', secure: false, changeOrigin: true });
+});
+
+// Doddgy: Level information
+app.use('/gm', function barMiddleware(req, res, next) {
+    proxy.web(req, res, { target: 'https://www.stockfighter.io/gm', secure: false, changeOrigin: true, headers: { Origin: 'www.stockfighter.io' } });
 });
 
 // What about permissions
